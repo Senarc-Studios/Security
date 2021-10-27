58e0ca38fd0ba708088165b81ace6116531390fa
Benitz Original <benitz@numix.xyz>
2021-10-27 16:02:47 +0800

    Fixed ANOTHER Typo

--- cogs/log.py
+++ cogs/log.py
@@ -24,7 +24,7 @@ class Log(commands.Cog):
 		_embed.set_author(name="Security Bot Events")
 		_embed.add_field(name="Event:", value=f"`ON_COMMAND`", inline=False)
 		_embed.add_field(name="Command:", value=ctx.command, inline=False)
-		_embed.add_field(name="User:", value=f"{ctx.author.name}#{ctx.author.discriminator}(`{ctx.author.id}`)", inlin=False)
+		_embed.add_field(name="User:", value=f"{ctx.author.name}#{ctx.author.discriminator}(`{ctx.author.id}`)", inline=False)
 		_embed.add_field(name="Time:", value=f"{discord.Timestamp.now()}", inline=False)
 		_embed.set_footer(text="Security Bot", icon_url=self.bot.user.display_avatar)
 		await events.send(embed=_embed)
