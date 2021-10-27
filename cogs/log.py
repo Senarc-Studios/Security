11f618f5ae8e2fcd9a0e7957632dabf31f560c65
Benitz Original <benitz@numix.xyz>
2021-10-27 15:56:54 +0800

    Fixed Errors

--- cogs/log.py
+++ cogs/log.py
@@ -33,13 +33,15 @@ class Log(commands.Cog):
 	async def on_command_error(self, ctx, error):
 		if isinstance(error, commands.errors.CommandNotFound):
 			return
+		e = "`"
+		n = "\n"
 		events = int(env("EVENTS"))
 		events = await self.bot.fetch_channel(events)
 		_embed = discord.Embed(colour=0x2F3136)
 		_embed.set_author(name="Security Bot Events")
 		_embed.add_field(name="Event:", value=f"`ON_COMMAND_ERROR`", inline=False)
 		_embed.add_field(name="Command:", value=ctx.command, inline=False)
-		_embed.add_field(name="Error:", value=f"```py\n{type(error) + '\n' + error + '\n' +  error.__traceback__}\n```", inline=False)
+		_embed.add_field(name="Error:", value=f"{e}{e}{e}py{n}{error + n +  error.__traceback__}{n}{e}{e}{e}", inline=False)
 		_embed.add_field(name="User:", value=f"{ctx.author.name}#{ctx.author.discriminator}(`{ctx.author.id}`)", inlin=False)
 		_embed.add_field(name="Time:", value=f"{discord.Timestamp.now()}", inline=False)
 		_embed.set_footer(text="Security Bot", icon_url=self.bot.user.display_avatar)
