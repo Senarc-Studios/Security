251b96b5d7d081b5782f3a0b72454e281efdf6b7
BenitzCoding <benitzcoding@yahoo.com>
2022-04-01 10:27:58 +0800

    Renaming `shortcode` to `globals`.

---
+++ utils/globals.py
@@ -0,0 +1,20 @@
+import discord
+
+def respond(interaction: discord.Interaction, message: str, ephemeral: bool=False):
+    return interaction.response.send_message(message, ephemeral=True)
+
+def author(interaction: discord.Interaction):
+    return interaction.user
+
+def env(variable: str):
+	env = os.getenv(variable)
+	if env == None:
+		Terminal.error(f"Environmental variable \"{variable}\" not found.")
+		return None
+	else:
+		return env
+
+def owner(user):
+    if user.id == env("OWNER_ID"):
+        return True
+    return False
